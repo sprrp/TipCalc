5ff74920c470bde562b92eb4594c448bc2b88484
Surendra Pandey <sprrp@outlook.com>
2020-09-16 19:12:00 -0700

    Initial Commit

---
+++ TipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalc
+//
+//  Created by Surendra Pandey on 9/16/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
